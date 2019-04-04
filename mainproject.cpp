ff21ce4dd932f2666e84274863010ffa35431635
Neel Karsanbhai <nkarsanb7682@gmail.com>
2019-04-03 19:10:09 -0600

    Cleaned out all old classes. File reading, and table creation functions need to be written.

--- mainproject.cpp
+++ mainproject.cpp
@@ -1,4 +1,16 @@
-int int main(int argc, char const *argv[])
+#include <iostream>
+#include <queue>
+#include <vector>
+#include "FileReading.hpp"
+
+int main()
 {
+  char cArr[5] = {'E', 'A', 'D', 'B', 'C'};
+  float iArr[5] = {2,3,4,5,6};
+  FileReading fr;
+  fr.readFile("TestFile.txt");
+
+  fr.postOrder(fr.getRoot());
+
   return 0;
 }
